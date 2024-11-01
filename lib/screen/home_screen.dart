0c90b593697a44312def8c615a7f9aa2d113d1fb
ilsong963 <ilsong963@naver.com>
2024-11-01 19:14:48 +0900

    image picker 추가

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,7 +1,10 @@
 import 'dart:async';
+import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:image_picker/image_picker.dart';
+
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
@@ -11,37 +14,53 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   final PageController _pageController = PageController();
+  List<Widget> imageList = [];
+
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
-  Timer.periodic(const Duration(seconds: 3), (timer) {
 
-    int? nextPage = _pageController.page?.toInt();
-    if(nextPage == null){
-      return;
-    }
+    _initializeImage();
 
-    if(nextPage >= 4) {
-      nextPage = 0;
-    }
-    else{
-      nextPage++;
-    }
-    _pageController.animateToPage(nextPage, duration: const Duration(milliseconds: 300), curve: Curves.ease);
-  });
+    Timer.periodic(const Duration(seconds: 3), (timer) {
+      int? nextPage = _pageController.page?.toInt();
+      print(nextPage);
+      if (nextPage == null) {
+        return;
+      }
+
+      if (nextPage >= imageList.length - 1) {
+        nextPage = 0;
+      } else {
+        nextPage++;
+      }
+      _pageController.animateToPage(nextPage, duration: const Duration(milliseconds: 300), curve: Curves.ease);
+    });
   }
+
+  _initializeImage() {
+    imageList = [1, 2, 3, 4, 5].map((number) => Image.asset('asset/img/image_$number.jpeg', fit: BoxFit.cover)).toList();
+  }
+
   @override
   Widget build(BuildContext context) {
     SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light);
     return Scaffold(
+        floatingActionButton: FloatingActionButton(onPressed: () => addImage(), child: const Icon(Icons.add)),
         body: PageView(
           controller: _pageController,
-          children: [1, 2, 3, 4, 5].map((number) => Image.asset('asset/img/image_$number.jpeg',
-              fit:BoxFit.cover)).toList(),
+          children: imageList,
         ));
   }
+
+  void addImage() async {
+    final ImagePicker picker = ImagePicker();
+    final XFile? image = await picker.pickImage(source: ImageSource.gallery);
+    if (image == null) {
+      return;
+    }
+
+    imageList.add(Image.file(File(image.path), fit: BoxFit.cover));
+  }
 }
-
-
-
