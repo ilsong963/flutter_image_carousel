37e5b810f7f890f5b149eafd94a32922e34de0b0
ilsong963 <ilsong963@naver.com>
2024-12-29 03:15:55 +0900

    구글 드라이브 파일 저장

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,13 +1,20 @@
 import 'dart:async';
+import 'dart:convert';
 import 'dart:developer';
 import 'dart:io';
 
+import 'package:external_path/external_path.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:googleapis/drive/v2.dart' as googledrive;
+import 'package:googleapis_auth/auth.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:http/http.dart' as http;
+import 'package:path_provider/path_provider.dart';
 
-import '../google/google_sign.dart';
+import '../auth/auth.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -55,8 +62,11 @@ class _HomeScreenState extends State<HomeScreen> {
   }
 
   _initializeImage() {
-    imageList = [1, 2, 3, 4, 5].map((number) => Image.asset('asset/img/image_$number.jpeg', fit: BoxFit.cover)).toList();
+    imageList =
+        [1, 2, 3, 4, 5].map((number) => Image.asset('asset/img/image_$number.jpeg', fit: BoxFit.cover)).toList();
   }
+
+  String? accessToken;
 
   @override
   Widget build(BuildContext context) {
@@ -114,16 +124,137 @@ class _HomeScreenState extends State<HomeScreen> {
         newImage = Image.network('https://picsum.photos/200/300', fit: BoxFit.cover);
         break;
       case ImageType.googleDrive:
-        BackUpRepository backUpRepository = BackUpRepository();
-        GoogleSignInAccount? googleSignInAccount = await backUpRepository.signIn();
-        print("!@#");
-        print(await backUpRepository.getDriveApi(googleSignInAccount!));
+        signInWithGoogle();
+        googledrive.FileList list = await getListDriveFiles(accessToken!);
 
-        print("123");
+        if (!mounted) return;
+
+        await showDialog(
+          context: context,
+          builder: (context) {
+            return AlertDialog(
+              title: const Text('Select File'),
+              content: SizedBox(
+                height: 300,
+                width: 300,
+                child: ListView.builder(
+                  itemCount: list.items!.length,
+                  itemBuilder: (context, index) {
+                    return ListTile(
+                      title: Row(
+                        children: [
+                          list.items![index].thumbnailLink != null
+                              ? Image.network(list.items![index].thumbnailLink!, width: 50, height: 50)
+                              : const SizedBox(width: 50, height: 50),
+                          Expanded(child: Text(list.items![index].title!))
+                        ],
+                      ),
+                      onTap: () async {
+                        showDialog(
+                            context: context, builder: (context) => const Center(child: CircularProgressIndicator()));
+
+                        File file = await downLoad(list.items![index]);
+                        newImage = Image.file(file, fit: BoxFit.cover);
+                        Navigator.pop(context);
+                      },
+                    );
+                  },
+                ),
+              ),
+            );
+          },
+        );
+        imageList.add(newImage!);
+
         break;
     }
-
-    imageList.add(newImage!);
   }
 
+
+  Future<void> signInWithGoogle() async {
+    final GoogleSignIn googleSignIn = GoogleSignIn(
+      scopes: [
+        'https://www.googleapis.com/auth/drive',
+      ],
+    );
+
+    //이미 로그인되어 있는 경우
+    googleSignIn.isSignedIn().then((isSignedIn) {
+      return;
+    });
+
+    final GoogleSignInAccount? googleUser = await googleSignIn.signIn();
+    if (googleUser == null) {
+      // 사용자가 로그인 취소
+      return;
+    }
+    final GoogleSignInAuthentication googleAuth = await googleUser.authentication;
+
+    final OAuthCredential credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth.accessToken,
+      idToken: googleAuth.idToken,
+    );
+
+    // Firebase 인증
+    await FirebaseAuth.instance.signInWithCredential(credential);
+
+    // Access Token 출력
+    print('Access Token: ${googleAuth.accessToken}');
+    accessToken = googleAuth.accessToken;
+  }
+
+  Future<googledrive.FileList> getListDriveFiles(String accessToken) async {
+    final credentials = AccessCredentials(
+      AccessToken('Bearer', accessToken, DateTime.now().add(const Duration(hours: 1)).toUtc()),
+      null, // Refresh token 필요 없을 경우 null
+      ['https://www.googleapis.com/auth/drive'],
+    );
+
+    final authClient = authenticatedClient(http.Client(), credentials);
+
+    // Google Drive API 객체 생성
+    final driveApi = googledrive.DriveApi(authClient);
+
+    // Google Drive 파일 목록 가져오기
+    return await driveApi.files.list();
+  }
+
+  Future<File> downLoad(googledrive.File driveFile) async {
+    final credentials = AccessCredentials(
+      AccessToken('Bearer', accessToken!, DateTime.now().add(const Duration(hours: 1)).toUtc()),
+      null, // Refresh token 필요 없을 경우 null
+      ['https://www.googleapis.com/auth/drive'],
+    );
+
+    final authClient = authenticatedClient(http.Client(), credentials);
+
+    // Google Drive API 객체 생성
+    final driveApi = googledrive.DriveApi(authClient);
+
+
+    googledrive.Media media = await driveApi.files
+        .get(driveFile.id!, downloadOptions: googledrive.DownloadOptions.fullMedia) as googledrive.Media;
+
+    List<int> data = [];
+
+    await media.stream.forEach((element) {
+      data.addAll(element);
+    });
+    String? downloadDirPath;
+
+    if (Platform.isAndroid) {
+      downloadDirPath = await ExternalPath.getExternalStoragePublicDirectory(ExternalPath.DIRECTORY_DOWNLOADS);
+      Directory dir = Directory(downloadDirPath);
+
+      if (!dir.existsSync()) {
+        downloadDirPath = (await getExternalStorageDirectory())!.path;
+      }
+    } else if (Platform.isIOS) {
+      downloadDirPath = (await getApplicationDocumentsDirectory()).path;
+    }
+    File file = File("${downloadDirPath!}/${driveFile.title!}");
+    file.writeAsBytesSync(data);
+
+    return file;
+  }
 }
